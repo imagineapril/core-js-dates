a6c2eeaeb8b39e94f7f9182a737e2cdaa4898c33
imagineapril <julia.timokhina604@gmail.com>
2025-03-23 19:56:24 +0300

    feat: add getTime function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,12 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  function addZero(num) {
+    if (num < 10) return `0${num}`;
+    return num;
+  }
+  return `${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`;
 }
 
 /**
